ac9f9d8acff4e17abfcf755d0e07f54bece50965
Zort <zort@ks3355756.kimsufi.com>
2014-09-21 13:25:08 +0200

    test

--- plugins/yo.py
+++ plugins/yo.py
@@ -3,6 +3,7 @@
 
 import os.path
 import saxo
+from random import choice
 
 @saxo.setup
 def setup(irc):
@@ -27,7 +28,15 @@ def deliver(irc):
             recipient = row[1]
             sender = row[0]
             message = row[4]
-            irc.say('"%s"' % message)
+            quote = choice(['The old man down the road says, "%s"',
+                            'A little bird said, "%s", and flew away.',
+                            "There's a rumour going around that %s",
+                            'There was a message in a bottle: "%s"',
+                            'The tea leaves spell out: "%s"',
+                            'I once heard this: "%s"',
+                            'Yo, by the way, %s',
+                            'At night, they howl and they cackle, and their words are: "%s"'])
+            irc.say(quote % message)
             del db["saxo_yo"][row]
 
 @saxo.event("JOIN")
