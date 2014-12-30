8250380cf4bb05e90cb8f78a53279ebf0e04b29b
Zort <eitanp32@gmail.com>
2014-12-29 20:13:49 -0500

    Added wololo() response, exclamation() without !

--- plugins/standard.py
+++ plugins/standard.py
@@ -5,7 +5,9 @@ import saxo
 
 @saxo.event("PRIVMSG")
 def exclamation(irc):
-    if irc.text == irc.config["nick"] + "!":
+    if irc.text == irc.config["nick"]:
+        irc.say(irc.nick)
+    elif irc.text == irc.config["nick"] + "!":
         irc.say(irc.nick + "!")
 
 # TODO: Move this into prefix.py?
@@ -13,3 +15,8 @@ def exclamation(irc):
 def prefix(irc):
     if irc.text == irc.config["nick"] + ": prefix?":
         irc.reply('"' + irc.config["prefix"] + '"')
+
+@saxo.event("PRIVMSG")
+def wololo(irc):
+    if irc.text in ["18", "29", "29?"]:
+        irc.say("30!")
